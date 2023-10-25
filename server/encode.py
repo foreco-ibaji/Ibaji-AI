e1dc9ccf57cf32031d018e6c2c6ed3194dc28aeb
Juntae <79131091+sronger@users.noreply.github.com>
2023-10-26 02:37:23 +0900

    refactor : encode 함수 수정 #11

--- server/encode.py
+++ server/encode.py
@@ -1,11 +1,12 @@
 import cv2
 import numpy as np
 import io
-from fastapi import Response
+import base64
 
 def image_encode(image):
     _, image_serial = cv2.imencode('.png', image)
-    image_bytes = io.BytesIO(image_serial.tobytes())
-    #image_bytes = image_serial.tobytes()
+    #image_bytes = io.BytesIO(image_serial.tobytes())
+    image_bytes = image_serial.tobytes()
+    encoded_string = base64.b64encode(image_bytes).decode()
     
-    return image_bytes
+    return encoded_string
